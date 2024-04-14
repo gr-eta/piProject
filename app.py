ae011a557df1598398bbed3ed9029fc46884f380
bellakamont <bkamont@uvm.edu>
2024-04-14 13:53:23 -0400

    updated html file

--- app.py
+++ app.py
@@ -2,6 +2,17 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
+def forward():
+    return "ok"
+def reverse():
+    return "ok"
+
+def left():
+    return "ok"
+
+def right():
+    return "ok"
+
 @app.route("/", methods=["GET"])
 def home():
     return render_template("main.html", title="Button", name="Prof. Eddy")
