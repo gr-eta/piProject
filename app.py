703df9aa91e9e3364657b96b02df28133c37a242
bellakamont <bkamont@uvm.edu>
2024-04-14 14:11:37 -0400

    updated html file

--- app.py
+++ app.py
@@ -6,7 +6,15 @@ app = Flask(__name__)
 def forward():
     return "ok"
 
+@app.route("/forward_off", methods=["POST"])
+def forward():
+    return "ok"
+
 @app.route("/reverse_on", methods=["POST"])
+def reverse():
+    return "ok"
+
+@app.route("/reverse_off", methods=["POST"])
 def reverse():
     return "ok"
 
@@ -14,11 +22,18 @@ def reverse():
 def left():
     return "ok"
 
+@app.route("/left_off", methods=["POST"])
+def left():
+    return "ok"
+
 @app.route("/right_on", methods=["POST"])
 def right():
     return "ok"
 
-@app.route("/led_on", methods=["POST"])
+@app.route("/right_off", methods=["POST"])
+def right():
+    return "ok"
+
 @app.route("/", methods=["GET"])
 def home():
     return render_template("main.html", title="Button", name="Prof. Eddy")
