a58baff1cb7e339f974b3a734bc1a067b2979d3e
bellakamont <bkamont@uvm.edu>
2024-04-14 14:23:37 -0400

    updated html file

--- app.py
+++ app.py
@@ -2,38 +2,6 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
-@app.route("/forward_on", methods=["POST"])
-def forward():
-    return "ok"
-
-@app.route("/forward_off", methods=["POST"])
-def forward():
-    return "ok"
-
-@app.route("/reverse_on", methods=["POST"])
-def reverse():
-    return "ok"
-
-@app.route("/reverse_off", methods=["POST"])
-def reverse():
-    return "ok"
-
-@app.route("/left_on", methods=["POST"])
-def left():
-    return "ok"
-
-@app.route("/left_off", methods=["POST"])
-def left():
-    return "ok"
-
-@app.route("/right_on", methods=["POST"])
-def right():
-    return "ok"
-
-@app.route("/right_off", methods=["POST"])
-def right():
-    return "ok"
-
 @app.route("/", methods=["GET"])
 def home():
-    return render_template("main.html", title="Button", name="Prof. Eddy")
+    return render_template("main.html")
