3139d831726a7a2e99a61ce4ba3fe1fe4e562a67
Bella <112650457+BellaKamont@users.noreply.github.com>
2024-04-24 17:45:51 -0400

    Update button.js

--- static/button.js
+++ static/button.js
@@ -4,41 +4,89 @@ var button3 = $("#left_button");
 var button4 = $("#right_button");
 
 button1.click(function() {
-    $.ajax({
-        url: "/forward",
-        type: "post",
-        success: function(response) {
-            console.log(response);
-        }
-    });
+    console.log(button1.text());
+    if (button1.text() === "Forward"){
+        $.ajax({
+            url: "/forward",
+            type: "post",
+            success: function(response) {
+                console.log(response);
+                button1.text("Forward Off");
+            }
+        });
+    } else{
+        $.ajax({
+            url: "/forward_off",
+            type: "post",
+            success: function(response) {
+                button1.text("Forward");
+            }
+        });
+    }
 });
 
 button2.click(function() {
-    $.ajax({
-        url: "/reverse",
-        type: "post",
-        success: function(response) {
-            console.log(response);
-        }
-    });
+    console.log(button2.text());
+    if (button2.text() === "Reverse"){
+        $.ajax({
+            url: "/reverse",
+            type: "post",
+            success: function(response) {
+                console.log(response);
+                button2.text("Reverse Off");
+            }
+        });
+    } else{
+        $.ajax({
+            url: "/reverse_off",
+            type: "post",
+            success: function(response) {
+                button2.text("Reverse");
+            }
+        });
+    }
 });
 
 button3.click(function() {
-    $.ajax({
-        url: "/left",
-        type: "post",
-        success: function(response) {
-            console.log(response);
-        }
-    });
+    console.log(button3.text());
+    if(button3.text() === "Left"){
+        $.ajax({
+            url: "/left",
+            type: "post",
+            success: function(response) {
+                console.log(response);
+                button3.text("Left Off");
+            }
+        });
+    } else {
+        $.ajax({
+            url: "/left_off",
+            type: "post",
+            success: function(response) {
+                button3.text("Left");
+            }
+        });
+    }
 });
 
 button4.click(function() {
-    $.ajax({
-        url: "/right",
-        type: "post",
-        success: function(response) {
-            console.log(response);
-        }
-    });
+    console.log(button4.text());
+    if(button4.text() === "Right"){
+        $.ajax({
+            url: "/right",
+            type: "post",
+            success: function(response) {
+                console.log(response);
+                button4.text("Right Off");
+            }
+        });
+    } else {
+        $.ajax({
+            url: "/right_off",
+            type: "post",
+            success: function(response) {
+                button3.text("Right");
+            }
+        });
+    }
 });
