62d48357dda626c540910a839e253f8addd6a9c9
Bella <112650457+BellaKamont@users.noreply.github.com>
2024-04-24 17:43:01 -0400

    Update app.py

--- app.py
+++ app.py
@@ -3,30 +3,58 @@ from gpiozero import Robot
 import time
 
 app = Flask(__name__)
-robot = Robot(left=(7,8), right=(9,10))
 
-@app.route("/", methods=["GET"])
-def home():
-    return render_template("main.html", title="Button", name="Prof. Eddy")
+robot = Robot(left=(9,10), right=(7,8))
+##motor1 = Motor(7, 8)
 
 @app.route("/forward", methods=["POST"])
 def forward():
+    print("Forward on")
     robot.forward()
-    time.sleep(1)
+    return "ok"
+
+@app.route("/forward_off", methods=["POST"])
+def forward_off():
+    print("Forward off")
     robot.stop()
     return "ok"
 
 @app.route("/reverse", methods=["POST"])
 def reverse():
+    print("Reverse on")
     robot.backward()
-    time.sleep(1)
+    return "ok"
+
+@app.route("/reverse_off", methods=["POST"])
+def reverse_off():
+    print("Reverse off")
     robot.stop()
     return "ok"
 
 @app.route("/left", methods=["POST"])
 def left():
+    print("Left on")
+    robot.left()
+    return "ok"
+
+@app.route("/left_off", methods=["POST"])
+def left_off():
+    print("Left off")
+    robot.stop()
     return "ok"
 
 @app.route("/right", methods=["POST"])
 def right():
+    print("Right on")
+    robot.right()
     return "ok"
+
+@app.route("/right_off", methods=["POST"])
+def right_off():
+    print("Right off")
+    robot.stop()
+    return "ok"
+
+@app.route("/", methods=["GET"])
+def home():
+    return render_template("main.html", title="Button", name="Prof. Eddy")
