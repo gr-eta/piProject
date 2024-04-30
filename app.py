15e196055b216ec675a26f1fc5e9db4368cf0532
Bella <112650457+BellaKamont@users.noreply.github.com>
2024-04-30 17:19:42 -0400

    Update app.py

--- app.py
+++ app.py
@@ -1,58 +1,68 @@
 from flask import Flask, render_template
 from gpiozero import Robot
-import time
+from gpiozero import Motor
 
 app = Flask(__name__)
 
+## side wheels
 robot = Robot(left=(7,8), right=(9,10))
-##motor1 = Motor(7, 8)
+# Third wheel
+motor3 = Motor(22,27)
 
 @app.route("/forward", methods=["POST"])
 def forward():
     print("Forward on")
     robot.forward()
+    motor3.forward()
     return "ok"
 
 @app.route("/forward_off", methods=["POST"])
 def forward_off():
     print("Forward off")
     robot.stop()
+    motor3.stop()
     return "ok"
 
 @app.route("/reverse", methods=["POST"])
 def reverse():
     print("Reverse on")
     robot.backward()
+    motor3.backward()
     return "ok"
 
 @app.route("/reverse_off", methods=["POST"])
 def reverse_off():
     print("Reverse off")
     robot.stop()
+    motor3.stop()
     return "ok"
 
 @app.route("/left", methods=["POST"])
 def left():
     print("Left on")
-    robot.left()
+    robot.right()
+    motor3.right()
     return "ok"
 
 @app.route("/left_off", methods=["POST"])
 def left_off():
     print("Left off")
     robot.stop()
+    motor3.stop()
     return "ok"
 
 @app.route("/right", methods=["POST"])
 def right():
     print("Right on")
-    robot.right()
+    robot.left()
+    motor3.left()
     return "ok"
 
 @app.route("/right_off", methods=["POST"])
 def right_off():
     print("Right off")
     robot.stop()
+    motor3.stop()
     return "ok"
 
 @app.route("/", methods=["GET"])
